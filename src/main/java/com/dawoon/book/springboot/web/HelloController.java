35994bc738d415118291a80811a12b8aec5b7a18
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-04 16:11:40 +0900

    test code 작성

--- src/main/java/com/dawoon/book/springboot/web/HelloController.java
+++ src/main/java/com/dawoon/book/springboot/web/HelloController.java
@@ -1,12 +1,19 @@
 package com.dawoon.book.springboot.web;
+import com.dawoon.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
 public class HelloController {
 
     @GetMapping("/hello") // HTTP Method인 Get의 Request를 받을 수 있는 API 생성
-    public String hello(){
+    public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello/dto") // ResponseDto를 사용하도록 하는 코드
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/dawoon/book/springboot/dto/HelloResponseDtoTest.java
+++ src/test/java/com/dawoon/book/springboot/dto/HelloResponseDtoTest.java
@@ -1,4 +1,5 @@
 package com.dawoon.book.springboot.dto;
+import com.dawoon.book.springboot.web.dto.HelloResponseDto;
 import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -7,5 +8,13 @@ public class HelloResponseDtoTest {
     public void 롬복_기능_테스트(){
         //given
         String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
     }
 }
