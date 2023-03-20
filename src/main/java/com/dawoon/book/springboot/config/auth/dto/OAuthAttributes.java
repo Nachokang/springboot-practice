70fb5c95ad78475ff218212ab4c0bf707bf6a3ac
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-20 17:07:32 +0900

    네이버 로그인 추가 구현

--- src/main/java/com/dawoon/book/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/dawoon/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,9 @@ public class OAuthAttributes {
 
     // OAuth2User에서 반환하는 사용자 정보는 Map이기 때문에 값 하나하나를 변환해야만 한다.
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -38,6 +41,17 @@ public class OAuthAttributes {
                 .build();
     }
 
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
     // User 엔티티를 생성한다.
     // OAuthAttributes에서 엔티티를 생성하는 시점은 처음 가입할 때 이다.
     // 가입할 때의 기본 권한을 GUEST로 주기 위해서 role 빌더 값에는 Role.GUEST를 사용한다.
