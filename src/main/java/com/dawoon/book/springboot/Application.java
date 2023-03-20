2a876c7958bd3ad565eeb780823497780e2e4450
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-20 17:38:41 +0900

    기존 Test에 시큐리티 적용하기 -> gradle 전체 테스트 통과!

--- src/main/java/com/dawoon/book/springboot/Application.java
+++ src/main/java/com/dawoon/book/springboot/Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing // JPA Auditing을 활성화해주는 Annotation
+// JPA Auditing을 활성화해주는 Annotation @EnableJpaAuditing이 삭제됨
 @SpringBootApplication
 public class Application {
     public static void main(String[] args){
