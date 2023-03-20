5c5ea4e64d16210a0974211c4030f7c2af1f1a3e
DaWoonKang <nachokang@ajou.ac.kr>
2023-03-20 16:08:30 +0900

    Google 로그인 테스트

--- src/main/java/com/dawoon/book/springboot/web/IndexController.java
+++ src/main/java/com/dawoon/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.dawoon.book.springboot.web;
 
+import com.dawoon.book.springboot.config.auth.dto.SessionUser;
 import com.dawoon.book.springboot.service.posts.PostsService;
 import com.dawoon.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // 로그인 성공 시 httpSession.getAttribute("user")에서 값을 가져올 수 있음
+        if(user!=null){ // 세션에 저장된 값이 있으면 model에 userName으로 등록, 없으면 로그인 버튼이 보임
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
